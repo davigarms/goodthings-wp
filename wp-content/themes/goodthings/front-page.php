1ecdbe5fda790f9e2cee60bf6c6afdb96a3f921a
Davi Garms <davigarms@gmail.com>
2021-03-29 15:40:44 +0100

    Add goodthings theme

---
+++ wp-content/themes/goodthings/front-page.php
@@ -0,0 +1,126 @@
+      <section class="banner">
+        <div class="img">
+            <img src="img/banner.jpg" alt="">
+        </div>
+        <div class="container">
+            <h2>Get online week 2021</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis elit lectus. Aliquam ac odio sit amet magna imperdiet.
+            </p>
+            <button class="contained rounded secondary">
+                <a href="">
+                    Get involved now!
+                </a>
+            </button>
+        </div>
+      </section>
+
+      <section class="about">
+          <div class="img">
+              <img src="img/woman.jpg" alt="">
+          </div>
+          <div class="content">
+              <h2>
+                  We are a social change charity, helping people to improve their lives through digital.
+              </h2>
+              <p>
+                  We tackle the pressing issues of our time, working with partners in thousands of communities across the UK and further afield.
+              </p>
+              <button class="outlined rounded secondary">
+                  <a href="">
+                      Learn more about us
+                  </a>
+              </button>
+          </div>
+      </section>
+
+      <section class="how">
+          <div class="container">
+              <h2>
+                  How can we help you?
+              </h2>
+              <p>
+                  Let us know who you are and what you're looking for, and we'll help get you to the right place.
+              </p>
+              <form id="howform" class="howform">
+                  <label for="user">I am</label>
+                  <select name="user" id="name" form="howform">
+                      <option value="individual">an Individual</option>
+                  </select>
+                  <label for="target">and I want to</label>
+                  <select name="target" id="target" form="howform">
+                      <option value="learn">learn</option>
+                  </select>
+                  <button class="contained rounded primary">
+                      <a href="">
+                          Start now
+                      </a>
+                  </button>
+              </form>
+          </div>
+      </section>
+
+      <section class="what">
+          <div class="container">
+              <h2>
+                  What do we do?
+              </h2>
+              <p>
+                  You might not have heard of us, but we're people behind the following impactful programmes.
+              </p>
+              <div class="content">
+                  <div class="box">
+                      <h3>
+                          Get online week
+                      </h3>
+                      <p>Lorem ipsum</p>
+                      <button class="outlined rounded primary">
+                          <a href="">
+                              Read more
+                          </a>
+                      </button>
+                  </div>
+                  <div class="box">
+                      <h3>
+                          Learn my way
+                      </h3>
+                      <p>Lorem ipsum</p>
+                      <button class="outlined rounded primary">
+                          <a href="">
+                              Read more
+                          </a>
+                      </button>
+                  </div>
+                  <div class="box">
+                      <h3>
+                          Make it click
+                      </h3>
+                      <p>Lorem ipsum</p>
+                      <button class="outlined rounded primary">
+                          <a href="">
+                              Read more
+                          </a>
+                      </button>
+                  </div>
+                  <div class="box">
+                      <h3>
+                          Digital you
+                      </h3>
+                      <p>Lorem ipsum</p>
+                      <button class="outlined rounded primary">
+                          <a href="">
+                              Read more
+                          </a>
+                      </button>
+                  </div>
+              </div>
+
+              <div class="call">
+                  <button class="contained rounded secondary">
+                      <a href="">
+                          More about what we do
+                      </a>
+                  </button>
+              </div>
+          </div>
+      </section>
---
+++ wp-content/themes/goodthings/header.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="css/main.css">
+      <title>Good Things Foundation</title>
+  </head>
+  <body>
+      <header>
+        <div class="topbar">
+            <div class="container">
+                <div class="search">
+                    <form>
+                        <input type="text" placeholder="Search" name="search">
+                    </form>
+                </div>
+                <div class="buttons">
+                    <button class="underlined secondary">
+                        <a href="">
+                            Learn
+                        </a>
+                    </button>
+                    <button class="contained primary">
+                        <a href="">
+                            Donate
+                        </a>
+                    </button>
+                </div>
+            </div>
+        </div>
+        
+        <div class="navbar">
+            <div class="container">
+                <div class="logo"></div>
+                <ul class="nav">
+                    <li>
+                        <a href="">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            What we do
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            The digital divide
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Get involved
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Our network
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Insights
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+      </header>      
