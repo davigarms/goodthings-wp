5118090b6a1cb78fcd41f61c0cdb851f740fc456
Davi Garms <davigarms@gmail.com>
2021-03-29 16:39:22 +0100

    Add page template

---
+++ wp-content/themes/goodthings/page.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+  <section class="empty">
+    <h1>
+      Lorem Ipsum
+    </h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quam arcu, aliquam a convallis viverra, molestie ut odio. Integer vulputate tempor fringilla. Nulla mi neque, ullamcorper sit amet porta eu, feugiat nec eros. Maecenas pharetra.
+    </p>
+  </section>
+<?php get_footer(); ?>
