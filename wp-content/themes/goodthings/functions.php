44fb26b5e1be7be8dd11f57d04408f5abe9b4429
Davi Garms <davigarms@gmail.com>
2021-03-29 16:00:06 +0100

    Add and register stylesheet

---
+++ wp-content/themes/goodthings/functions.php
@@ -0,0 +1,11 @@
+<?php
+  function load_stylesheets() {
+    wp_register_style('styles', get_template_directory_uri() . '/css/main.css', array(), 1, 'all');
+    wp_enqueue_style('styles');
+
+    wp_register_style('custom', get_template_directory_uri() . '/custom.css', array(), 1, 'all');
+    wp_enqueue_style('custom');
+  }
+
+  add_action('wp_enqueue_scripts', 'load_stylesheets');
+?>
--- wp-content/themes/goodthings/header.php
+++ wp-content/themes/goodthings/header.php
@@ -6,8 +6,9 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="preconnect" href="https://fonts.gstatic.com">
       <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
-      <link rel="stylesheet" href="css/main.css">
       <title>Good Things Foundation</title>
+
+      <?php wp_head(); ?>
   </head>
   <body>
       <header>
