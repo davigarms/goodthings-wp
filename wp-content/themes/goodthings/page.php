40e6e66a9dbf017305823ad957bd09a8753b99aa
Davi Garms <davigarms@gmail.com>
2021-03-30 19:39:57 +0100

    add wck custom fields plugin

--- wp-content/themes/goodthings/page.php
+++ wp-content/themes/goodthings/page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
   <section class="empty">
     <h1>
-      Lorem Ipsum
+      Wordpress site
     </h1>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quam arcu, aliquam a convallis viverra, molestie ut odio. Integer vulputate tempor fringilla. Nulla mi neque, ullamcorper sit amet porta eu, feugiat nec eros. Maecenas pharetra.
